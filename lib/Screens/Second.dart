99d1c277b8d514ddd69a7075901eccba56243ff8
“Tarun <tarunrathod00805@gmail.com>
2023-12-13 15:55:12 +0530

    updated second and third content

--- lib/Screens/Second.dart
+++ lib/Screens/Second.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:navbar/Widgets/info_card.dart';
 class Second extends StatefulWidget {
   const Second({super.key});
 
@@ -14,9 +14,42 @@ class _SecondState extends State<Second> {
       appBar: AppBar(
         automaticallyImplyLeading: true,
       ),
-      body: Center(
-        child: Text('This is the Second Page'),
-      ),
+        body: Column(
+          children: [
+            InfoCard(name: "Saras Bagh", place: "Venue lime, Pune"),
+            InfoCard(name: "Saras Bagh", place: "Venue lime, Pune"),
+            InfoCard(name: "Saras Bagh", place: "Venue lime, Pune"),
+            InfoCard(name: "Saras Bagh", place: "Venue lime, Pune"),
+            InfoCard(name: "Saras Bagh", place: "Venue lime, Pune"),
+
+
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      "https://res.cloudinary.com/drpj8yeqp/image/upload/v1702401134/Uphaar_App/Uphaar_App_Logo-removebg-preview_wvrpvd.png",
+                      width: 50,
+                    ),
+                    const Text(
+                      "TEAM UPHAAR",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 5
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        )
     );
   }
 }
